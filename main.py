1e9ffea31fbc1cd5581239ff0c198656d3c43a14
So7eN <soorenaaryan7@protonmail.com>
2023-12-26 20:20:46 +0330

    Setting up the Menu
    Main window and importing modules

---
+++ main.py
@@ -0,0 +1,14 @@
+# Modules
+from tkinter import *
+import tkinter.filedialog as fd
+import tkinter.messagebox as mb
+import tkinter.colorchooser as cc
+import os
+
+# Main Window
+win = Tk()
+win.title("Untitled - Notepad")
+win.geometry("800x500")
+win.resizable()
+win.update()
+win.mainloop()
