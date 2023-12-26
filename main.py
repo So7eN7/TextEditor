954642eb770b534bc82fa50017564384ecefc983
So7eN <soorenaaryan7@protonmail.com>
2023-12-26 20:35:14 +0330

    Frame/Textarea
    Making of textarea/scrollbar

--- main.py
+++ main.py
@@ -10,5 +10,20 @@ win = Tk()
 win.title("Untitled - Notepad")
 win.geometry("800x500")
 win.resizable()
+
+# Main Frame to set our text area
+frame = Frame(win)
+frame.pack(pady=7)
+
+# Scrollbar(vertical)
+scroll_v = Scrollbar(frame)
+scroll_v.pack(side=RIGHT, fill=Y)
+
+# Textarea
+textarea = Text(frame, width=100, height=25, font=("Arial", 17), selectbackground="cyan", selectforeground="black"
+                ,undo=True, yscrollcommand=scroll_v.set)
+textarea.pack()
+scroll_v.config(command=textarea.yview)
+
 win.update()
 win.mainloop()
